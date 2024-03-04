3cbf1b8cc8378b364dbfb79b42bd6247269cb847
strate95 <wfpjhw95@gmail.com>
2024-03-04 10:50:22 +0900

    중간 저장

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,27 @@
 public class Main {
 
-    public static void main() {
-        sssss
+    // 자바 프로그램 시작과 종료 지점
+    public static void main(String[] args) {
+        // 출력
+        System.out.println(100);
+        System.out.println(200);
+
+        // 프로그램은 위에서 아래로 순차적 실행
+
+        // 문자 출력
+        System.out.println("홍성재");
+        System.out.println("홍길동");
+
+        // 숫자, 문자, 아무거나 10개정도 출력
+        System.out.println("0405");
+        System.out.println("0526");
+        System.out.println("문자");
+        System.out.println("바나나");
+        System.out.println("딸기");
+        System.out.println("망고");
+        System.out.println("참외");
+        System.out.println("국밥");
+        System.out.println("제육");
+        System.out.println("내 사랑");
     }
 }
